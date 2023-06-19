1bad0382ac7ffe5ea3c8f47d4238964db927647e
becastro <benatcastro@gmail.com>
2023-06-19 11:39:40 +0000

    relauncher script

---
+++ srcs/requirements/nginx/run.sh
@@ -0,0 +1,19 @@
+IMAGE_NAME="nginx_test"
+CONTAINER_NAME="nginx_server"
+PORTS="80:80"
+
+docker rm --force $(docker ps -aq)
+
+# Remove pre created images
+
+docker rmi --force nginx_test
+
+# Build new one
+
+docker build -t $IMAGE_NAME .
+
+# Run
+
+docker run -d --name $CONTAINER_NAME -p $PORTS $IMAGE_NAME
+
+
