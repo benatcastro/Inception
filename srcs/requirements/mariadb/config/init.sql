0056aa05fb8b29e59e292a728acacd64372e3115
becastro <benatcastro@gmail.com>
2023-07-25 18:32:16 +0000

    php installed in wordpress dockerfile

--- srcs/requirements/mariadb/config/init.sql
+++ srcs/requirements/mariadb/config/init.sql
@@ -2,10 +2,10 @@
 
 CREATE DATABASE wordpress;
 
---set root pass
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('placeholder_root_password');
-
 --create new user
 CREATE USER 'placeholder_username'@'localhost' IDENTIFIED BY 'placeholder_password';
 GRANT ALL PRIVILEGES ON mydatabase.wordpress TO 'placeholder_username'@'localhost';
+
+--set root pass
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root1234');
 
