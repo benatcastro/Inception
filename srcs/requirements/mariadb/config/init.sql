6522ef15c546571ee7535364ba30648e533c170d
becastro <benatcastro@gmail.com>
2023-09-09 17:20:14 +0000

    wp core install and mariadb connection solved

--- srcs/requirements/mariadb/config/init.sql
+++ srcs/requirements/mariadb/config/init.sql
@@ -1,9 +1,10 @@
 --create db
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root1234');
 
-CREATE DATABASE wordpress;
+CREATE DATABASE IF NOT EXISTS wordpress;
+
 
 --create new user
-CREATE USER 'placeholder_username'@'%' IDENTIFIED BY 'placeholder_password';
+CREATE USER IF NOT EXISTS 'placeholder_username'@'%' IDENTIFIED BY 'placeholder_password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'placeholder_username'@'%';
-FLUSH PRIVILEGES;
+
+SET PASSWORD FOR 'root'@'%' = PASSWORD('root1234');
--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -2,12 +2,18 @@ cd /var/www/html
 
 
 wp core download
-wp config create 	--dbname=${WORDPRESS_DB_NAME} \
-			--dbuser=${MYSQL_USER} \
-			--dbpass=${MYSQL_PASSWORD} \
+wp config create 	--dbname=${WORDPRESS_DB_NAME} 		\
+			--dbuser=${MYSQL_USER} 			\
+			--dbpass=${MYSQL_PASSWORD} 		\
 			--dbhost=${WORDPRESS_DB_HOST} --skip-check
+
+wp core install 	--url=example.com			\
+			--title=${WORDPRESS_TITLE}		\
+			--admin_user=${WORDPRESS_USERNAME}	\
+			--admin_password=${WORDPRESS_PASSWORD}	\
+			--admin_email=${WORDPRESS_EMAIL}	\
+			--skip-email
 
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0;/g' /etc/php81/php.ini
 
-#sleep infinity
 php-fpm81 --nodaemonize
